3833f0be09839581582d8f8d014e81995650f0c4
mattdesl <dave.des@gmail.com>
2016-03-11 16:53:02 -0500

    fix division by zero error on some windows devices

--- index.glsl
+++ index.glsl
@@ -4,7 +4,7 @@
 float grain(vec2 texCoord, vec2 resolution, float frame, float multiplier) {
     vec2 mult = texCoord * resolution;
     float offset = snoise3D(vec3(mult / multiplier, frame));
-    float n1 = pnoise3D(vec3(mult, offset), vec3(1.0/texCoord * resolution, 0.0));
+    float n1 = pnoise3D(vec3(mult, offset), vec3(1.0/texCoord * resolution, 1.0));
     return n1 / 2.0 + 0.5;
 }
 
